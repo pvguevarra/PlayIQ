a1e83432065d725b8afb468f51b6ed9579cf5d70
Patrick Guevarra <pvguevarra@csuchico.edu>
2025-02-13 19:22:16 -0800

    Added button and profile pic to home page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,22 +8,96 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Color purple = Color(0xFF800080);
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Column(
         children: [
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            children: [
-            Text("Welcome Coach", style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Colors.black),),
-            SizedBox(width: 10),
-          ],
-          ), 
-        ),
-      ],
-       ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Welcome Coach",
+                      style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 125),
+                CircleAvatar(
+                  radius: 27,
+                  backgroundImage: const AssetImage('assets/images/luka.jpg'),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          // ignore: deprecated_member_use
+                          color: purple.withOpacity(0.2),
+                          blurRadius: 15,
+                          spreadRadius: 5,
+                          offset: Offset(-0, 10),
+                        ),
+                      ],
+                      color: purple,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Icon(
+                          Icons.add_circle,
+                          size: 60,
+                          color: Colors.white,
+                        ),
+                        SizedBox(height: 40),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "View Roster",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: -1,
+                              ),
+                            ),
+                            Text(
+                              "Make an edit to your roster",
+                              style: TextStyle(
+                                color: Colors.white38,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
